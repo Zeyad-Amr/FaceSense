8cafee45f28d828ed32348ca601572aaae2b2e0e
Ahmed Abd El-Raouf <araouf481@hotmail.com>
2023-05-14 16:42:39 +0300

    solve detect images

--- main.cpp
+++ main.cpp
@@ -1,36 +1,39 @@
-#include <set>
-#include "k-means.h"
+
 #include <opencv2/opencv.hpp>
-#include <cmath>
 #include <vector>
+#include <opencv2/opencv.hpp>
 
-
-int main(int argc, char *argv[])
+int main()
 {
-
+    // Load the cascade classifier XML file for face detection
     cv::CascadeClassifier faceCascade;
-    faceCascade.load("haarcascade_frontalface_alt.xml");
+    faceCascade.load("C:/opencv/sources/data/haarcascades_cuda/haarcascade_frontalface_default.xml");
 
-    cv::Mat image = cv::imread("02.jfif");
-
-//     if you want to convert to grayscale
-//    cv::Mat grayImage;
-//    cv::cvtColor(image, grayImage, cv::COLOR_BGR2GRAY);
-
-    std::vector<cv::Rect> faces;
-    faceCascade.detectMultiScale(grayImage, faces, 1.1, 3, 0, cv::Size(30, 30));
-
-    for (const cv::Rect& faceRect : faces)
+    // Load the image file
+    cv::Mat image = cv::imread("D:/SBME/ComputerVision/Final-Project/myWorkSpace/01.jpg");
+    if (image.empty())
     {
-        cv::rectangle(image, faceRect, cv::Scalar(0, 255, 0), 2);
+        std::cout << "Failed to open the image file." << std::endl;
+        return -1;
     }
 
+    // Convert the image to grayscale for face detection
+    cv::Mat grayImage;
+    cv::cvtColor(image, grayImage, cv::COLOR_BGR2GRAY);
+
+    // Perform face detection
+    std::vector<cv::Rect> faces;
+    faceCascade.detectMultiScale(grayImage, faces, 1.1, 3, 0 | cv::CASCADE_SCALE_IMAGE, cv::Size(30, 30));
+
+    // Draw rectangles around the detected faces
+    for (const auto &face : faces)
+    {
+        cv::rectangle(image, face, cv::Scalar(0, 255, 0), 2);
+    }
+
+    // Display the image with detected faces
     cv::imshow("Face Detection", image);
-//    cv::imshow("Face Detection", grayImage);
     cv::waitKey(0);
 
-
-
     return 0;
-
 }
