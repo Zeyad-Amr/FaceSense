10f7a492c3d9876fa41f728519fd1be3e97f76a2
michaelhany510 <69060386+michaelhany510@users.noreply.github.com>
2023-05-17 00:57:28 +0300

    merging with zeyad done

---
+++ qt/FaceRecognition/mypca.cpp
@@ -0,0 +1,164 @@
+#include "MyPCA.h"
+
+MyPCA::MyPCA(const Mat data, int maxComponents)
+{
+    MatrixXd eigen_data = cvMatToEigen(data);
+    numSamples = eigen_data.rows();
+    numFeatures = eigen_data.cols();
+    mean = calculateMean(eigen_data);
+    normalizedData = normalizeData(eigen_data, mean);
+
+    string resultsFile = "pca_results.txt";
+
+    ifstream file(resultsFile);
+    if (file.peek() == ifstream::traits_type::eof())
+    {
+        cout << "File is empty, storing ..." << endl;
+        // File is empty, calculate and store the selectedEigenVectors
+        covariance = calculateCovariance(normalizedData);
+        eigenSolver = calculateEigenSolver(covariance);
+        selectedEigenVectors = selectTopEigenVectors(eigenSolver.eigenvectors(), maxComponents);
+
+        storeSelectedEigenVectors(selectedEigenVectors, resultsFile);
+    }
+    else
+    {
+        cout << "File is not empty, loading ..." << endl;
+        // File is not empty, load the selectedEigenVectors from the file
+
+        loadSelectedEigenVectors(resultsFile, selectedEigenVectors);
+    }
+}
+
+Mat MyPCA::reduceData(const Mat data)
+{
+
+    MatrixXd eigen_data = cvMatToEigen(data);
+
+    numSamples = eigen_data.rows();
+    numFeatures = eigen_data.cols();
+    mean = calculateMean(eigen_data);
+    normalizedData = normalizeData(eigen_data, mean);
+
+    cout << "Data Dimension: " << numSamples << " x " << numFeatures << endl;
+
+    Mat reducedMat = eigenToCvMat(normalizedData * selectedEigenVectors);
+
+    cout << "Reduced Data Dimension: " << reducedMat.rows << " x " << reducedMat.cols << endl;
+
+    return reducedMat;
+}
+
+VectorXd MyPCA::calculateMean(const MatrixXd &data)
+{
+    return data.colwise().mean();
+}
+
+MatrixXd MyPCA::normalizeData(const MatrixXd &data, const VectorXd &mean)
+{
+    return data.rowwise() - mean.transpose();
+}
+
+MatrixXd MyPCA::calculateCovariance(const MatrixXd &normalizedData)
+{
+    return (normalizedData.transpose() * normalizedData) / (numSamples - 1);
+}
+
+SelfAdjointEigenSolver<MatrixXd> MyPCA::calculateEigenSolver(const MatrixXd &covariance)
+{
+    return SelfAdjointEigenSolver<MatrixXd>(covariance);
+}
+
+MatrixXd MyPCA::selectTopEigenVectors(const MatrixXd &eigenVectors, int maxComponents)
+{
+    return eigenVectors.rightCols(maxComponents);
+}
+
+MatrixXd MyPCA::cvMatToEigen(const cv::Mat &cvMat)
+{
+    cout << "start convert to eigen" << endl;
+    cv::Mat cvMatDouble;
+    cvMat.convertTo(cvMatDouble, CV_64FC1);
+
+    Eigen::MatrixXd eigenMat(cvMatDouble.rows, cvMatDouble.cols);
+    Eigen::Map<Eigen::MatrixXd>(eigenMat.data(), eigenMat.rows(), eigenMat.cols()) = Eigen::Map<const Eigen::MatrixXd>(cvMatDouble.ptr<double>(), cvMatDouble.rows, cvMatDouble.cols);
+
+    cout << "end convert to eigen" << endl;
+    return eigenMat;
+}
+
+Mat MyPCA::eigenToCvMat(const Eigen::MatrixXd &eigenMat)
+{
+    cout << "start convert to mat" << endl;
+
+    cv::Mat cvMat(eigenMat.rows(), eigenMat.cols(), CV_64FC1);
+    Eigen::Map<Eigen::MatrixXd>(cvMat.ptr<double>(), cvMat.rows, cvMat.cols) = eigenMat;
+
+    // Convert training and test data to CV_32F
+    cv::Mat cvMat32F;
+    cvMat.convertTo(cvMat32F, CV_32F);
+
+    cout << "end convert to mat" << endl;
+    return cvMat32F;
+}
+
+void MyPCA::storeSelectedEigenVectors(const MatrixXd &selectedEigenVectors, const string &filename)
+{
+    ofstream file(filename);
+    if (file.is_open())
+    {
+        for (int i = 0; i < selectedEigenVectors.rows(); i++)
+        {
+            for (int j = 0; j < selectedEigenVectors.cols(); j++)
+            {
+                file << selectedEigenVectors(i, j) << " ";
+            }
+            file << "\n";
+        }
+        file.close();
+        cout << "Selected EigenVectors stored in " << filename << endl;
+    }
+    else
+    {
+        cout << "Unable to open file " << filename << " for storing the selected EigenVectors." << endl;
+    }
+}
+
+void MyPCA::loadSelectedEigenVectors(const string &filename, MatrixXd &selectedEigenVectors)
+{
+    ifstream file(filename);
+    if (file.is_open())
+    {
+        vector<vector<double>> data;
+        string line;
+        while (getline(file, line))
+        {
+            vector<double> row;
+            istringstream iss(line);
+            double value;
+            while (iss >> value)
+            {
+                row.push_back(value);
+            }
+            data.push_back(row);
+        }
+        file.close();
+
+        int rows = data.size();
+        int cols = (rows > 0) ? data[0].size() : 0;
+        selectedEigenVectors.resize(rows, cols);
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < cols; j++)
+            {
+                selectedEigenVectors(i, j) = data[i][j];
+            }
+        }
+
+        cout << "Selected EigenVectors loaded from " << filename << endl;
+    }
+    else
+    {
+        cout << "Unable to open file " << filename << endl;
+    }
+}
---
+++ qt/FaceRecognition/mypca.h
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <cmath>
+#include <eigen-git-mirror/Eigen/Dense>
+#include <opencv2/opencv.hpp>
+using namespace Eigen;
+using namespace std;
+using namespace cv;
+
+class MyPCA
+{
+public:
+    MyPCA(const Mat data, int maxComponents);
+    Mat reduceData(const Mat data);
+
+private:
+    int numSamples;
+    int numFeatures;
+    VectorXd mean;
+    MatrixXd normalizedData;
+    MatrixXd covariance;
+    SelfAdjointEigenSolver<MatrixXd> eigenSolver;
+    MatrixXd selectedEigenVectors;
+
+    VectorXd calculateMean(const MatrixXd &data);
+    MatrixXd normalizeData(const MatrixXd &data, const VectorXd &mean);
+    MatrixXd calculateCovariance(const MatrixXd &normalizedData);
+    SelfAdjointEigenSolver<MatrixXd> calculateEigenSolver(const MatrixXd &covariance);
+    MatrixXd selectTopEigenVectors(const MatrixXd &eigenVectors, int maxComponents);
+    Eigen::MatrixXd cvMatToEigen(const cv::Mat &cvMat);
+    Mat eigenToCvMat(const MatrixXd &eigenMat);
+    void storeSelectedEigenVectors(const MatrixXd &selectedEigenVectors, const string &filename);
+    void loadSelectedEigenVectors(const string &filename, MatrixXd &selectedEigenVectors);
+};
